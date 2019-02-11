2b885751444cd8dc63acbd941989685eb074caba
Monika Patel <monikapatel.it@outlook.com>
2019-02-11 18:03:35 +1300

    Updates

---
+++ precache-manifest.27f5de0c64f03b95c1d3afa88a2a41c0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "https//monikapatelit.github.io/weather-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "4cbd8148c6e43a0da2b6",
+    "url": "https//monikapatelit.github.io/weather-app/static/css/main.282906cb.chunk.css"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "https//monikapatelit.github.io/weather-app/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "cbda27e7128cacbe9180",
+    "url": "https//monikapatelit.github.io/weather-app/static/js/1.cbda27e7.chunk.js"
+  },
+  {
+    "revision": "3fc4dab71ba3f0d9daed",
+    "url": "https//monikapatelit.github.io/weather-app/static/js/runtime~main.3fc4dab7.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "https//monikapatelit.github.io/weather-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "4cbd8148c6e43a0da2b6",
+    "url": "https//monikapatelit.github.io/weather-app/static/js/main.4cbd8148.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "https//monikapatelit.github.io/weather-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "https//monikapatelit.github.io/weather-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "https//monikapatelit.github.io/weather-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "cbda27e7128cacbe9180",
+    "url": "https//monikapatelit.github.io/weather-app/static/css/1.7dbf47aa.chunk.css"
+  },
+  {
+    "revision": "a96123b23b41d8df5d68cf134135d0fc",
+    "url": "https//monikapatelit.github.io/weather-app/index.html"
+  }
+];
